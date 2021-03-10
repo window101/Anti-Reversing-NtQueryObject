8769767d0a01154ca39d3cfd50a4e4b07cf18c23
window101 <61288242+window101@users.noreply.github.com>
2021-03-10 18:33:36 +0900

    Update Self-Creation.c

--- Self-Creation.c
+++ Self-Creation.c
@@ -1,2 +1,36 @@
 
-#include windo
+#include windows.h
+#include tchar.h
+#include stdio.h
+
+void ChildProc() {
+  MessageBox(NULL, L"This is a child process!", L"DebugMe2", MB_OK);
+  ExitProcess(0);
+}
+void _tmain(int argc, TCHAR *argv[]) {
+  
+  TCHAR szPath[MAX_PATH] = {0, };
+  STARTUPINFO si = {sizeof(STARTUPINFO), };
+  PROCESS_INFORMATION pi = {0,};
+  CONTEXT ctx = {0,};
+  
+  _tprintf(L"This is a parent process\n");
+  GetMoudleFileName(NULL, szPath, sizeof(TCHAR) * MAX_PATH);
+  
+  CreateProcess(szPath, NULL, NULL, NULL, FALSE,
+                CREATE_SUSPENDED,
+                NULL, NULL, &si, &pi);
+  ctx.ContextFlags = CONTEXT_FULL;
+  
+  GetThreadContext(pi.hThread, &ctx);
+  ctx.Eip = (DWORD)ChildProc;
+  
+  SetThreadContext(pi.hThread, &ctx);
+  
+  ResumeThread(pi.hThread);
+  
+  WaitForSingleObject(pi.hProcess, INFINITE);
+  
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+}
